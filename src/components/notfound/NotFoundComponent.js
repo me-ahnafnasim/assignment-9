c9fdc55faff96a6c118b6c2cd56a097f35f2d29d
me-ahnafnasim <108536275+me-ahnafnasim@users.noreply.github.com>
2023-09-17 01:23:24 +0600

    Update NotFoundComponent.js
    404 setup

--- src/components/notfound/NotFoundComponent.js
+++ src/components/notfound/NotFoundComponent.js
@@ -4,7 +4,7 @@ const NotFoundComponent = () => {
     return (
         <div className='flex justify-center items-center'>
             <div className='mt-30'>
-            {/* <h1 className='text-xl'>Opps! page not found</h1>  */}
+           
            <img className='w-500 h-50 mt-9' src='https://cdn.dribbble.com/users/707244/screenshots/5086963/anushka.jpg' alt='404'/>
             </div>
         </div>
